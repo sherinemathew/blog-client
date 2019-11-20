d8fbca2cbddbfaab0b4e58ed692fbf14215f9bfb
sherinemathew <sherine.mary.mathew@gmail.com>
2019-11-20 11:52:52 +1000

    Updates

---
+++ precache-manifest.6bbb7129f280531370e46cbf706f0d50.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a17a1e948f224cf9cf2c1ba23fa922d5",
+    "url": "/blog-client/index.html"
+  },
+  {
+    "revision": "7e23c8f691d4fb0e2c62",
+    "url": "/blog-client/static/css/2.46f0bf41.chunk.css"
+  },
+  {
+    "revision": "7e23c8f691d4fb0e2c62",
+    "url": "/blog-client/static/js/2.0a48bea9.chunk.js"
+  },
+  {
+    "revision": "503b8ebc3ba124a12cfa",
+    "url": "/blog-client/static/js/main.92c86c60.chunk.js"
+  },
+  {
+    "revision": "0a2fb8f7e4203d6b14ed",
+    "url": "/blog-client/static/js/runtime-main.2689d353.js"
+  }
+]);
